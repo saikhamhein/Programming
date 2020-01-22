7040d99d1c161e5719011a4c786bc0ac2c9901a4
sai <saikhamhein@gmail.com>
2020-01-22 09:51:12 +0800

    Simple C code that uses CreatFile to open a serial port

---
+++ Stones/SerialPort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+
+int main(void)
+{
+    HANDLE hComm;
+
+    hComm = CreateFile("\\\\.\\COM30",               //Portname
+                       GENERIC_READ | GENERIC_WRITE, //Read and Write
+                       0,                            //NO Sharing
+                       NULL,                         //No security
+                       OPEN_EXISTING,                // Open Existing Port Only
+                       0,                            //Non Overlapped IO
+                       NULL);                        //NULL for COM devices
+
+    if (hComm == INVALID_HANDLE_VALUE)
+        printf("Error in opening serial port\r\n");
+    else
+        printf("opening serial port successful\r\n");
+
+    CloseHandle(hComm); //Closing the Serial Port
+
+        return 0;
+}
